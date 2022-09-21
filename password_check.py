4f4593fe31e736d33eefe67a200c2111733574a3
Tarun3322 <tarun.chevula@thundersoft.com>
2022-09-21 17:19:22 +0530

    Made changes in password check program
    Change-Id: Ia0949161c7c30d16708e79ed2be7de03c95dde7c

--- password_check.py
+++ password_check.py
@@ -2,7 +2,7 @@ A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 a = "abcdefghijklmnopqrstuvwxyz"
 num = "1234567890"
 sp = "!@#$%^&*()_+-=;:'\""
-inp = "Qwerty@00"
+inp = input("Enter your password with a small letters, capital letters, special character and size of 8: ")
 x = y = z = s = False
 
 
@@ -17,7 +17,7 @@ for i in inp:
         s = True
 
 
-if x == True and y == True and z == True and s == True:
+if x == True and y == True and z == True and s == True and len(inp) >= 8:
     print("Valid Password")
 else:
     print("Inavalid Password")

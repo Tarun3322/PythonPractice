7f4c16dc20e216e47cbbbb19f266be730bfe9f2a
Tarun3322 <tarun.chevula@thundersoft.com>
2022-11-11 11:49:53 +0530

    adding class practice file

---
+++ class_practice.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self, name, id, location):
+        self.name = name
+        self.id = id
+        self.location = location
+
+    def prnt(self):
+        print(self.name + " is working in " + self.location + " location" + ", whose id is " + self.id + ".")
+
+emp1 = Employee("Tarun", "0210487", "HYD")
+emp2 = Employee("Maha","10811","VSKP")
+
+emp1.prnt()
+emp2.prnt()
